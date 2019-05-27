24cec51d7021c7c8bafaf09a5a21d647b7f9f61b
monykeyup <monykeyup@163.com>
2019-05-27 17:27:46 +0800

    idea commit

---
+++ src/com/company/Test01.java
@@ -0,0 +1,13 @@
+package com.company;
+
+/**
+ * @描述： TODO
+ * @创建人： dev
+ * @创建时间： 2019/5/2717:25
+ * @邮箱：monykeyup@163.com
+ */
+public class Test01 {
+    public static void main(String[] args) {
+        System.out.println("更新！！！");
+    }
+}
